6b1d348bccd3552232446fb7c0a93298407dabee
Flffy-Oriole <aarrtteemm2006@gmail.com>
2026-06-26 14:38:01 +0800

    notFound page улучшена

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,9 +1,11 @@
 import styles from './NotFound.module.css'
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
     return (
-        <>
-        <h1 className={styles.notFound}>Not Found</h1>
-        </>
+        <div className={styles.notFoundContainer}>
+            <h1 className={styles.notFound}>Такой страницы не существует</h1>
+            <Link to="/" className={styles.link}>Вернуться на главную</Link>
+        </div>
     )
 }
