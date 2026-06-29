b2abe75d26c402005020a65ce4ed9cb22d0f72c9
Flffy-Oriole <aarrtteemm2006@gmail.com>
2026-06-29 22:28:25 +0800

    изменен вид profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,11 +1,31 @@
 import styles from './Profile.module.css';
+import { authClient } from "../lib/authClient";
+import { useTranslation } from 'react-i18next';
 
 export default function Profile() {
+    const i18n = useTranslation();
+    const data = authClient.useSession();
+
+    if (data.isPending) {
+        return (
+            <div>Загрузка...</div>
+        )
+    }
+
+    const userData = data.data.user;
+    console.log(userData);
+
     return (
         <div className={styles.mainContainer}>
-            <h1 className={styles.text}>Name: John Doe</h1>
-            <p className={styles.email}>Email: john.doe@example.com</p>
-            <button className={styles.changePasswordButton}>Change Password</button>
+            <div className={styles.infoBox}>
+                <span className={styles.commonText}>{i18n.t("name")}</span>
+                <span className={styles.personalInfo}>{ userData.name }</span>
+                <span className={styles.commonText}>{i18n.t("email")}</span>
+                <span className={styles.personalInfo}> {userData.email}</span>
+                <span>{i18n.t("password")}</span>
+                <button className={styles.changePasswordButton}>{i18n.t("changePassword")}</button>
+            </div>
+            <button className={styles.logOutBtn}>{i18n.t("logout")}</button>
         </div>
     )
 }
