9f2f5dc1a1df57c9316d03300f022e8d1435e9e7
Flffy-Oriole <aarrtteemm2006@gmail.com>
2026-07-14 00:40:03 +0800

    mvp heropage

--- frontend/src/pages/HeroPage.tsx
+++ frontend/src/pages/HeroPage.tsx
@@ -1,13 +1,48 @@
 import styles from './HeroPage.module.css';
 import { useTranslation } from 'react-i18next';
+import GreenButton from '../components/GreenButton';
+import EButton from '../components/EButton';
+import indexImg from '../assets/indeximg.jpg';
+import { useNavigate } from "react-router-dom";
+
 
 export default function HeroPage() {
     const { i18n } = useTranslation();
+    const navigate = useNavigate();
+    
     return (
         <>
-            <h1>{i18n.t("heroPageTitle")}</h1>
-            <button>{i18n.t("register")}</button>
-            <button>{i18n.t("goShopping")}</button>
+            <div className={styles.hero}>
+                <div className={styles.heroContent}>
+                    <h1 className={styles.title}>
+                        {i18n.t("heroPageTitle")}
+                    </h1>
+
+                    <p className={styles.subtitle}>
+                        {i18n.t("heroPageDescription")}
+                    </p>
+
+                    <div className={styles.actions}>
+                        <GreenButton
+                            text={i18n.t("goShopping")}
+                            onClick={() => {navigate("/home")}}
+                        />
+
+                        <EButton
+                            text={i18n.t("register")}
+                            onClick={() => navigate("/registration")}
+                        />
+                    </div>
+                </div>
+
+                <div className={styles.imageBlock}>
+                    <img 
+                        className={styles.heroImage}
+                        src={indexImg}
+                        alt="index image"
+                    />
+                </div>
+            </div>
         </>
     )
 }
