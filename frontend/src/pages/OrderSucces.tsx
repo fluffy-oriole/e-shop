2ae29cca30c05ec267adee4a1119a182538c8234
Flffy-Oriole <aarrtteemm2006@gmail.com>
2026-07-13 23:05:35 +0800

    стилизована страница после оплаты

--- frontend/src/pages/OrderSucces.tsx
+++ frontend/src/pages/OrderSucces.tsx
@@ -1,15 +1,24 @@
 import { Link } from 'react-router-dom';
-import styles from './OrderSuccess.module.css';
 import { useTranslation } from 'react-i18next';
+import GreenButton from '../components/GreenButton';
+import styles from './OrderSuccess.module.css';
 
 export default function OrderSuccess() {
     const { i18n } = useTranslation();
 
     return (
         <div className={styles.container}>
-            <h1>{i18n.t("orderSuccess")}</h1>
-            <p>{i18n.t("thanks")}</p>
-            <Link to="/">{i18n.t("goHome")}</Link>
+            <div className={styles.card}>
+                <h1>{i18n.t("orderSuccess")}</h1>
+                <p>{i18n.t("thanks")}</p>
+
+                <Link to="/">
+                    <GreenButton
+                        text={i18n.t("goHome")}
+                        onClick={() => {}}
+                    />
+                </Link>
+            </div>
         </div>
     );
 }
